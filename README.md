54a4806ce379b04bc918b0eaed917a475b97d675
trwy7 <47907719+trwy7@users.noreply.github.com>
2026-03-25 19:19:36 -0600

    feat: First working commit! Outlines the config

---
+++ README.md
@@ -0,0 +1,9 @@
+# Friend elector
+
+A bot for large friend group servers (10+ people).
+
+## Important
+
+Updating the bot may require manually editing the configuration file. After an update, always check the bots logs.
+
+This bot requires that you have a discord alt account. This alt account needs to own the server, your main should not have admin permissions.
---
+++ main.py
@@ -0,0 +1,116 @@
+from typing import cast
+import os
+import sys
+import shutil
+import yaml
+import logging
+import discord
+from uwuipy import Uwuipy
+
+# Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("elector")
+
+# App config
+def validate_conf(source: dict, against: dict):
+    for reqk, reqv in against.items():
+        if reqk in source:
+            if type(reqv) == type(source[reqk]): # pylint: disable=unidiomatic-typecheck # i dont want to include subclasses
+                if isinstance(reqv, dict):
+                    validate_conf(source[reqk], reqv)
+            else:
+                raise ValueError(f"{reqk} exists in your config, but is not the same type as the example file")
+        else:
+            raise ValueError(f"{reqk} is not in your config, copy it from the example file")
+
+def load_config():
+    with open("conf.example.yml", "r", encoding="UTF-8") as dc:
+        default_config = yaml.safe_load(dc)
+
+    if os.path.exists("data/config.yml"): # begone, windows developers
+        with open("data/config.yml", "r", encoding="UTF-8") as c:
+            tconf = yaml.safe_load(c)
+            validate_conf(source=tconf, against=default_config)
+            return tconf
+    else:
+        shutil.copyfile("conf.example.yml", "data/config.yml")
+        print("Default config has been created")
+        sys.exit(1)
+
+config = load_config()
+
+# Library setup
+
+uwulib = Uwuipy(
+    None,
+    action_chance=0
+)
+
+# Global vars
+
+SERVER = None
+
+ANNOUNCE_CHANNEL: discord.TextChannel = None
+VOICE_CHANNEL: discord.VoiceChannel = None
+LOG_CHANNEL: discord.TextChannel = None
+VOICE_CATEGORY: discord.CategoryChannel = None
+VOTE_CATEGORY: discord.CategoryChannel = None
+
+LEADER_ROLE = None
+VICE_ROLE = None
+VIP_ROLE = None
+PLUS_ROLE = None
+GUEST_ROLE = None
+
+# Bot setup
+
+init_complete = False
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = discord.Bot(intents=intents)
+
+@bot.event
+async def on_ready():
+    global init_complete, ANNOUNCE_CHANNEL, VOICE_CHANNEL, LOG_CHANNEL, VOICE_CATEGORY, VOTE_CATEGORY, LEADER_ROLE, VICE_ROLE, VIP_ROLE, PLUS_ROLE, GUEST_ROLE, SERVER
+    logger.info('Logged in as %s', bot.user)
+
+    ANNOUNCE_CHANNEL = bot.get_channel(config['channels']['public']) # type: ignore # these return the right type, but pycord dosent know that
+    VOICE_CHANNEL = bot.get_channel(config['channels']['voice']) # type: ignore
+    LOG_CHANNEL = bot.get_channel(config['channels']['logs']) # type: ignore
+    VOICE_CATEGORY = bot.get_channel(config['channels']['voice_rooms_category']) # type: ignore
+    VOTE_CATEGORY = bot.get_channel(config['channels']['vote_category']) # type: ignore
+
+    SERVER = ANNOUNCE_CHANNEL.guild
+
+    LEADER_ROLE = SERVER.get_role(config['roles']['leader'])
+    VICE_ROLE = SERVER.get_role(config['roles']['vice-leader'])
+    VIP_ROLE = SERVER.get_role(config['roles']['vip_role'])
+    PLUS_ROLE = SERVER.get_role(config['roles']['plus_role'])
+    GUEST_ROLE = SERVER.get_role(config['roles']['guest_role'])
+
+    await bot.sync_commands()
+    init_complete = True
+
+# Functions
+
+async def public_log(embed: discord.Embed):
+    await ANNOUNCE_CHANNEL.send(embed=embed)
+
+async def admin_log(embed: discord.Embed):
+    await LOG_CHANNEL.send(embed=embed)
+
+# Commands
+
+@bot.slash_command(name="ping", description="Make sure the bot is online")
+@discord.default_permissions(administrator=True)
+async def ping(ctx: discord.ApplicationContext):
+    await ctx.respond("Pong!", ephemeral=True)
+
+# Background
+
+# Let's run!
+
+bot.run(config['token'])
